f909d47833604e6a7a1443ebf5e88d73b87ebd37
Begzod <152687654+assamarqandiy@users.noreply.github.com>
2023-12-02 20:10:56 +0500

    Create 0003.cpp
    Solution of 0003 problem in ACMP.ru in C++.
    https://acmp.ru/index.asp?main=task&id_task=3

---
+++ 0003.cpp
@@ -0,0 +1,9 @@
+// Five to five - twenty five!   (Пятью пять - двадцать пять!)
+// Time Complexity O(1)
+#include <iostream>
+int main(){
+    long long a;
+    std::cin >> a;
+    std::cout << a*a;
+    return 0;
+}
