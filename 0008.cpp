c230ef77e2ed88657e5b3272e555a8b9fc6fbcf5
Begzod <134162756+Sayfiddinzoda@users.noreply.github.com>
2023-12-06 13:09:31 +0500

    Create 0008.cpp
    Solution of 0008 problem in ACMP.ru (in C++)

---
+++ 0008.cpp
@@ -0,0 +1,10 @@
+// Arithmetic (Арифметика)
+// Time Complexity O(1)
+
+#include <iostream>
+int main(){
+    int a,b,c;
+    std::cin >> a >> b >> c;
+    std::cout << (a*b==c ? "YES":"NO");
+    return 0;
+}
